386ede67ddc3d040f27e1aaedfefe20fd8bf9a4e
thowi <thomas@wittek.me>
2020-05-22 13:42:31 +0200

    Update button analyzer.

--- deconz-lqi-analyze-button-events.py
+++ deconz-lqi-analyze-button-events.py
@@ -8,22 +8,28 @@ import sys
 from collections import defaultdict
 
 
-LOG_PATTERN = re.compile(r'([\d:]+).*uniqueid":"([\da-f:]+).*')
+LOG_PATTERN = re.compile(r'([\d:]+).*changed.*uniqueid":"([\da-f:]+).*')
+TIMESTAMP_PATTERN = '%H:%M:%S:%f'
 
 
-num_presses_by_time_and_device = defaultdict(lambda: defaultdict(int))
+presses = []
 with open(sys.argv[1], 'rt') as f:
   for line in f:
     match = LOG_PATTERN.match(line)
     if match:
-      timestamp, id_hex = match.groups()
-      ts = timestamp[:6]
-      num_presses_by_time_and_device[ts][id_hex] += 1
+      timestamp_str, id_hex = match.groups()
+      timestamp = datetime.datetime.strptime(timestamp_str, TIMESTAMP_PATTERN)
+      presses.append((timestamp, id_hex))
 
-all_devices = sorted(set(itertools.chain(
-    *[v.keys() for v in num_presses_by_time_and_device.values()])))
-sorted_dates = sorted(num_presses_by_time_and_device.keys())
+all_devices = sorted(set([p[1] for p in presses]))
+sorted_presses = sorted(presses, key=lambda p: p[0])
+last_press_by_device = {}
 print('Date;' + ';'.join(all_devices))
-for date in sorted_dates:
-  cols = [date] + [num_presses_by_time_and_device[date].get(d, '') for d in all_devices]
+for press in sorted_presses:
+  timestamp, id_hex = press
+  last_press = last_press_by_device.get(id_hex, timestamp)
+  seconds_since = (timestamp - last_press).total_seconds()
+  col_index = 1 + all_devices.index(id_hex)
+  cols = [timestamp] + [seconds_since if d == id_hex else '' for d in all_devices]
   print(';'.join([str(c) for c in cols]))
+  last_press_by_device[id_hex] = timestamp
