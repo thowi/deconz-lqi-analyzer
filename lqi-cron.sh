3a834e452aadf3e42653344c1d93fdc60dc20ea6
thowi <thomas@wittek.me>
2020-05-27 17:10:02 +0200

    Clean up old CSV files in cron.

--- lqi-cron.sh
+++ lqi-cron.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd && cd lqi/deconz-lqi-plugin && ./gviz_crt.py && cp lqi.csv lqi-`date +%Y-%m-%d-%H-%M-%S`.csv
 find . -mmin +59 -and -name "lqi-*.png" -delete
+find . -mtime +7 -and -name "lqi-*.csv" -delete
